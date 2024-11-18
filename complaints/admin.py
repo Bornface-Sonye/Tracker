fe1e6958c0ccd585e8b7df578c91385a64589eb6
Bornface-Sonye <bornfacesonye@gmail.com>
2024-11-18 15:38:57 -0800

    Admin.py changed

--- complaints/admin.py
+++ complaints/admin.py
@@ -1,7 +1,7 @@
 # complaints/admin.py
 from django.contrib import admin
 from .models import ( School, Department, Course, Student, Lecturer, Unit, NominalRoll,
-Response, LecturerUnit, Result, Complaint, System_User, AcademicYear
+Response, LecturerUnit, Result, Complaint, System_User, AcademicYear, PasswordResetToken
 )
 
 admin.site.register(NominalRoll)
@@ -17,3 +17,4 @@ admin.site.register(Unit)
 admin.site.register(LecturerUnit)
 admin.site.register(System_User)
 admin.site.register(AcademicYear)
+admin.site.register(PasswordResetToken)
